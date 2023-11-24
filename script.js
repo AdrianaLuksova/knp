0b6b60c785d3ba2f398b1cc55537bec7f6b1ce7c
Adriana Luksová <120632555+AdrianaLuksova@users.noreply.github.com>
2023-11-24 11:52:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+
+const gameContainer = document.querySelector(".container"),
+  userResult = document.querySelector(".user_result img"),
+  cpuResult = document.querySelector(".cpu_result img"),
+  result = document.querySelector(".result"),
+  optionImages = document.querySelectorAll(".option_image");
+
+optionImages.forEach((image, index) => {
+  image.addEventListener("click", (e) => {
+    image.classList.add("active");
+
+    userResult.src = cpuResult.src = "img/kamen.jpg";
+    result.textContent = "Wait...";
+
+    optionImages.forEach((image2, index2) => {
+      index !== index2 && image2.classList.remove("active");
+    });
+
+    gameContainer.classList.add("start");
+
+    let time = setTimeout(() => {
+      gameContainer.classList.remove("start");
+
+      let imageSrc = e.target.querySelector("img").src;
+      userResult.src = imageSrc;
+
+      let randomNumber = Math.floor(Math.random() * 3);
+      let cpuImages = ["img/kamen.jpg", "img/papir.jpg", "img/nuzky.jpg"];
+      cpuResult.src = cpuImages[randomNumber];
+
+      let cpuValue = ["R", "P", "S"][randomNumber];
+      let userValue = ["R", "P", "S"][index];
+
+      let outcomes = {
+        RR: "REMÍZA",
+        RP: "AI",
+        RS: "HRÁČ",
+        PP: "REMÍZA",
+        PR: "HRÁČ",
+        PS: "AI",
+        SS: "REMÍZA",
+        SR: "AI",
+        SP: "HRÁČ",
+      };
+
+      let outComeValue = outcomes[userValue + cpuValue];
+
+      result.textContent = userValue === cpuValue ? "Remíza" : `${outComeValue} Vyhrál!!`;
+    }, 2500);
+  });
+});
